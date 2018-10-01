52cb34041a865fee6f8bce3dcbf446932a86902f
Jay J <mis@thewoodagency.com>
2018-10-01 09:37:45 -0500

    Build Question Form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -37,7 +37,9 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create', compact('question'));
     }
 
     /**
---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>Ask Question</h2>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to
+                                    All</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <form action="{{ route('questions.store') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="question-title">Title</label>
+                                <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" id="question-title" name="title">
+                                @if ($errors->has('title'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $error->first('title') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="question-body">Question</label>
+                                <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                                @if ($errors->has('body'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $error->first('body') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
