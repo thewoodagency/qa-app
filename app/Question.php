3ba06089576d4dd355a31d8265844731b86b6227
Jay J <mis@thewoodagency.com>
2018-10-03 09:59:36 -0500

    Create Answer table and rename answer column to answer_count in the Questions table

--- app/Question.php
+++ app/Question.php
@@ -32,7 +32,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if ($this->best_answer_id) {
                 return "answered-accpeted";
             }
@@ -45,6 +45,11 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 
 }
