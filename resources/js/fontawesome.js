d3f6de32b05625bbf1a1bbce5434c8d196011eb5
Jay J <mis@thewoodagency.com>
2018-10-04 15:23:45 -0500

    Add vote controls on the show question page

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,3 @@
+import fontawesome from '@fortawesome/fontawesome-free';
+import from '@fortawesome/fontawesome-svg-core';
+import faCaretUp from '@fortawesome/free-solid-svg-icons';
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,17 +5,31 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h1>{{ $question->title }}</h1>
-                            <div class="ml-auto">
-                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to
-                                    All</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{ $question->title }}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to
+                                        All</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-body">
+                        <hr>
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="This question is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This question is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Click to mark as a favorite" class="favorite mt-2 favorited">
+                                    <i class="fas fa-star fa-2x"></i>
+                                    <span class="favorites-count">123</span>
+                                </a>
+                            </div>
                             <div class="media-body">
                                 <p class="lead">
                                     Asked by
@@ -42,7 +56,7 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-3">
+        <div class="row mt-4">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
@@ -52,6 +66,18 @@
                         <hr>
                         @foreach($question->answers as $answer)
                             <div class="media">
+                                <div class="d-fex flex-column vote-controls">
+                                    <a title="This answer is useful" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="votes-count">1230</span>
+                                    <a title="This answer is not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                </div>
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
                                     <div class="float-right">
