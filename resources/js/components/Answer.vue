155a0893dac8de0cde722d8ff8090ed87d31ce90
Jay J <mis@thewoodagency.com>
2018-10-18 10:49:16 -0500

    Implement a flashing message with iziToast

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -14,7 +14,7 @@
         },
 
         computed: {
-            isInvalid () {
+            isInvalid() {
                 console.log(this.body.length);
                 return this.body.length < 10;
             },
@@ -42,26 +42,54 @@
                         console.log('res', res);
                         this.editing = false;
                         this.bodyHtml = res.data.body_html
+                        this.$toast.success(res.data.message, "Success", {timeout: 3000});
                     }
                 ).catch(err => {
-                    alert(err.response.data.message);
+                    this.$toast.error(err.response.data.message, "Error", {timeout: 3000});
                 });
             },
 
             deleteAnswer() {
-                if (confirm('Are you sure?')) {
-                    axios.delete(this.endpoint)
-                        .then(
-                            res => {
-                                $(this.$el).fadeOut(500, () => {
-                                    alert(res.data.message);
+                this.$toast.question('Are you sure?', 'confirm', {
+                    timeout: 20000,
+                    close: false,
+                    overlay: true,
+                    displayMode: 'once',
+                    id: 'question',
+                    zindex: 999,
+                    title: 'Hey',
+                    position: 'center',
+                    buttons: [
+                        ['<button><b>YES</b></button>', (instance, toast) => {
+
+                            axios.delete(this.endpoint)
+                                .then(
+                                    res => {
+                                        $(this.$el).fadeOut(500, () => {
+                                            this.$toast.success(res.data.message, "Success", {timeout: 3000});
+                                        })
+                                    }
+                                )
+                                .catch(err => {
+                                    this.$toast.error(err.response.data.message, "Error", {timeout: 3000});
                                 })
-                            }
-                        )
-                        .catch(err => {
-                            alert(err.response.data.message);
-                        })
-                }
+
+                            instance.hide({transitionOut: 'fadeOut'}, toast, 'button');
+
+                        }, true],
+                        ['<button>NO</button>', function (instance, toast) {
+
+                            instance.hide({transitionOut: 'fadeOut'}, toast, 'button');
+
+                        }],
+                    ],
+                    onClosing: function (instance, toast, closedBy) {
+                        console.info('Closing | closedBy: ' + closedBy);
+                    },
+                    onClosed: function (instance, toast, closedBy) {
+                        console.info('Closed | closedBy: ' + closedBy);
+                    }
+                });
             }
         }
     }
